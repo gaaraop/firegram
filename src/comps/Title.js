662c76405f5b23c81eab33db8c9419ec8862dbb2
Aditya Narayn <aditya.narayn30@gmail.com>
2020-12-31 15:49:07 +0530

    Final

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,8 +4,8 @@ const Title = () => {
   return (
     <div className="title">
       <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h2>Your Collection</h2>
+      <p>A place where you Upload your photos!</p>
     </div>
   )
 }
